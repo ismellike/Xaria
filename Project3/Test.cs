4808667773440bc37434dcf2ed24be408cbf05af
Chris <chrissxxv@gmail.com>
2017-11-24 16:20:00 -0600

    2 intermediate enemy png's, drop png's

---
+++ Project3/Test.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input.Touch;
+using System;
+using System.Collections.Generic;
+using Xaria.Drops;
+using Xaria.Enemies;
+using Xaria.Projectiles;
+
+public class Test
+{
+	public Test()
+	{
+	}
+}
