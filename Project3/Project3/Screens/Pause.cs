92eb0293ee5691f179b1595803de64d500f03113
Shuai Deng <s039d249@ku.edu>
2017-11-28 17:15:57 -0600

    add pause function, two buttons in start screen and end screen, and sounds effect

---
+++ Project3/Project3/Screens/Pause.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Android.OS;
+using System.IO;
+
+namespace Xaria.Screens
+{
+    class Pause : Screen
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Start" /> class.
+        /// </summary>
+        /// <param name="Content">The content.</param>
+        public Pause(ContentManager Content)
+        {
+            Texture2D startTexture = Content.Load<Texture2D>("Buttons/pause");
+            Buttons.Add(new Button(startTexture, new Vector2(100, 0), PauseGame));
+        }
+        void PauseGame()
+        {
+
+        }
+    }
+}
